b46ef83c146bcbd4bc920ab8fef2a25f94d7b781
PUJAN POUDEL <pujanpoudel057@example.com>
2024-01-23 18:58:49 +0545

    js-basic - variable

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("hello world");
+
+//Variables (var , let , const) //  camelCase Letter
+
+var broadwayTeacher = "Pujan Poudel";  //es5
+
+broadwayTeacher = "Pawan";
+
+
+console.log(broadwayTeacher);
+
+
+// let , const (ES6)
+let mernTeacher = "raktim";
+
+console.log(mernTeacher);
+
+//const 
+const mernSir = "raktim";
+//mernSir = "pujan"; cant change
+console.log(mernSir);
+
+
+
+// create 4 variable useing const keyword for table, fan , chair , pen
+ const table = "Computer Table";
+ const fan = "Table Fan";
+ const chari = "Computer chair";
+ const pen = "Red Pen";
