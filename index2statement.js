9de33319941e6a04202054a594e7a9c15ac66fb7
PUJAN POUDEL <pujanpoudel057@example.com>
2024-01-25 00:04:05 +0545

    statement if/else

---
+++ index2statement.js
@@ -0,0 +1,10 @@
+// write an program that welcome use based on their name and pass input
+
+const userName = prompt("enter your username");
+const password = prompt("enter your password");
+
+if(userName==="pujan" && password === "admin"){
+    console.log("wecome Admin , pujan");
+}else{
+    console.log("invalid CODE");
+}
