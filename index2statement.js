9d009e743509cb0b0d877ae51b6bde68fef333d3
PUJAN POUDEL <pujanpoudel057@example.com>
2024-01-25 18:38:56 +0545

    task

--- index2statement.js
+++ index2statement.js
@@ -1,10 +1,15 @@
 // write an program that welcome use based on their name and pass input
 
-const userName = prompt("enter your username");
-const password = prompt("enter your password");
+// const userName = prompt("enter your username");
+// const password = prompt("enter your password");
 
-if(userName==="pujan" && password === "admin"){
-    console.log("wecome Admin , pujan");
-}else{
-    console.log("invalid CODE");
-}
+// if(userName=== password ){
+//     console.log("wecome Admin , pujan");
+// }else{
+//     console.log("invalid CODE");
+// }
+
+
+/// ternery statement 
+const months = prompt("enter the months");
+months === "jan" ? alert("jan") : months === "feb" ? alert("feb") : months === "march" ? alert("march") : alert("END OF MONTHS");
---
+++ task01.js
@@ -0,0 +1,12 @@
+// ask user for name and time (number)
+
+const name = prompt("your name?");
+const time = prompt("time - ");
+
+if(time >= 5  && time <=12 ){
+    alert(`Good Moring ${name}`);
+}else if(time >= 12  && time <=3 ){
+    alert(`Afternoon ${name}`);
+}else{
+    alert(`evening ${name}`);
+}
