46ef0538d95a901584ae43d5d9469ea00bb33e8d
PUJAN POUDEL <pujanpoudel057@example.com>
2024-01-25 22:27:14 +0545

    table using for , do while , while loop

---
+++ task02.js
@@ -0,0 +1,29 @@
+
+//  . for loop , while loop , dowhile loop 
+
+//for while
+let x = 1
+for (let i = 1; i<=10; i++){
+
+    console.log(`${x} * ${i} = ${x*i}`)
+}
+
+//while
+console.log("------while--------");
+
+let t= 9;
+let j=1;
+while(j <=10 ){
+    console.log(`${t} * ${j} = ${t*j}`)
+    j++;
+
+} 
+
+//do-while
+console.log("-----do-while--------");
+let k = 1
+let ta = 5;
+do{
+    console.log(`${ta} * ${k} = ${k*ta}`)
+    k++;
+}while (k <=10)
