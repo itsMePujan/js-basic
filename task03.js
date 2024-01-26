25b8b292584a5d2d1c12f90932e4682d11dd53b6
PUJAN POUDEL <pujanpoudel057@example.com>
2024-01-26 18:41:52 +0545

    task03

---
+++ task03.js
@@ -0,0 +1,9 @@
+//Write a JavaScript program to check whether a given positive number is a multiple of 3 or 7.
+
+const pNum = prompt("Enter the number:?")
+
+if(pNum%3 && pNum%7){
+    console.log(`${pNum} is not a multiple of either 3 or 7`)
+}else{
+    console.log('is multiply by 3 or 7')
+}
