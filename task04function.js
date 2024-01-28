ec70ebb5d1091cfe982aa8fa6936243ed8c82052
Pujan Poudel <wizard@Pujans-MacBook-Air.local>
2024-01-29 02:06:16 +0545

    reverse

--- task04function.js
+++ task04function.js
@@ -18,3 +18,25 @@ const area = (l,b)=>{
 
 const areaRect = area(5,3);
 console.log(areaRect);
+
+//reverse
+
+const reverse = (x) =>{
+ let newNum = "";
+ const str = String(x);
+ for(let i = str.length-1; i>=0;i--){
+    newNum += str[i];
+ }
+    return newNum;
+};
+
+const res = (reverse("pujan"));
+console.log(res);
+
+//// commaFromatter , example 10000 t0 10,000
+
+const comaFromatter = (x)=>{
+  let newValu = "";
+  const str = String(x);
+
+}
