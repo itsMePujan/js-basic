153728e926d12601b38ac6a8306faf931a906a6f
Pujan Poudel <wizard@Pujans-MacBook-Air.local>
2024-01-30 00:20:03 +0545

    func

--- task04function.js
+++ task04function.js
@@ -5,22 +5,15 @@ const multi = (tab) =>{
          console.log(`${tab} * ${i} = ${tab*i}`);
     }
 }
-
 multi(2);
-
-
 //area of rectangle 
-
 const area = (l,b)=>{
 
     return l*b;
 }
-
 const areaRect = area(5,3);
 console.log(areaRect);
-
 //reverse
-
 const reverse = (x) =>{
  let newNum = "";
  const str = String(x);
@@ -29,18 +22,14 @@ const reverse = (x) =>{
  }
     return newNum;
 };
-
 const res = (reverse("pujan"));
 console.log(res);
-
 //// commaFromatter , example 10000 t0 10,000
-
 const comaFromatter = (x)=>{
   let newValu = "";
   const str = String(x);
 
 }
-
 ///proper case
 const convertToTitleCase=(str) => {
     if (!str) {
@@ -50,3 +39,27 @@ const convertToTitleCase=(str) => {
   }
 
   console.log(convertToTitleCase("my name is pujan "));
+
+  //vowels in string 
+
+  const checkVowels = (str)=>{
+     const len= str.length;
+     let count = 0;
+     for(let i =0; i < len; i++){
+        if(str[i].includes('a') || str[i].includes('e') || str[i].includes('i')|| str[i].includes('o')|| str[i].includes('u')){
+         count = count+1;
+        }
+     }
+     console.log(`it count ${count} vowels`);
+  }
+  console.log(checkVowels("aeiou"));
+/// email validate 
+const emailCheck = (txt) => {
+    let email = txt;
+   if(!email.includes('@') || !email.includes('.com') ){
+        console.log("email not validate")
+   }else{
+    console.log("email validate");
+   }
+}
+emailCheck("pujanpoudel@.com");
