6fa5c7fbd88792edb74e00d4cee2e59829775aa8
itsMePujan <69003888+itsMePujan@users.noreply.github.com>
2024-02-02 16:52:00 +0545

    functions

---
+++ task06array.js
@@ -0,0 +1,34 @@
+//array
+
+const userRole = ["user", "manager", "vendor"];
+
+const sysRole = ["admin", "vendor"];
+
+const checkRole = (ur, sr) => sr.some((role) => ur.includes(role));
+console.log(checkRole(userRole, sysRole));
+
+/// function to sum array
+
+const arrySum = [1, 2, 3, 4, 5];
+
+const sumArray = (g) => {
+  let sum = 0;
+
+  g.forEach((x) => {
+    sum += x;
+  });
+
+  return sum;
+};
+
+console.log(sumArray(arrySum));
+
+///slugger
+const slugger = (word) =>
+  word
+    .toString()
+    .toLowerCase()
+    .replaceAll(/!@#$%^&"'*()_+/g, "")
+    .replaceAll(" ", "-");
+
+console.log(slugger("my name is pujan poudel"));
